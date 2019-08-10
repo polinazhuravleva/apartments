d5d38cafaddb8dc0fbe981e9294d9c8e2d9b72f0
polinazhuravleva <zhuravlevapolina@gmail.com>
2019-08-10 17:39:31 +0300

    webpack commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
     entry: {
-        main: path.resolve(__dirname, 'src/index/index.js')
+        main: path.resolve(__dirname, 'src/index.js')
     },
     output: {
         path: path.resolve(__dirname, 'docs'),
@@ -14,7 +15,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.(svg|ttf|woff)$/,
+                test: /\.(woff2?|ttf)$/,
                 loader: 'file-loader',
                 options: {
                     name: '[name].[ext]',
@@ -22,7 +23,7 @@ module.exports = {
                 }
             },
             {
-                test: /\.s?css$/,
+                test: /\.sass$/,
                 use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
             },
             {
@@ -48,7 +49,19 @@ module.exports = {
                         presets: ['@babel/env']
                     }
                 }
-            }
+            },
+            {
+                type: 'javascript/auto',
+                test: /\.json$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'apt_data/'
+                    }
+                }
+            },
         ]
     },
     externals: {
@@ -56,18 +69,21 @@ module.exports = {
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: 'style.css',
+            filename: '[name].css',
         }),
         new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.pug'),
             filename: 'index.html',
-            chunks: ['index'],
-            template: path.resolve(__dirname, 'src/index/index.pug')
+            //chunks: ['index'],
         }),
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery",
             "window.jQuery": "jquery"
-        })
+        }),
+        new CopyPlugin([
+            { from: 'src/components/content/apartments/img', to: 'images' }
+        ])
     ],
     devServer: {
         contentBase: path.join(__dirname, 'docs'),
