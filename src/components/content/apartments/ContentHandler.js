f8902d806c21f0236544d8e5ffc3eaf133359e49
polinazhuravleva <zhuravlevapolina@gmail.com>
2019-08-12 02:34:00 +0300

    apartment cards

--- src/components/content/apartments/ContentHandler.js
+++ src/components/content/apartments/ContentHandler.js
@@ -1,4 +1,5 @@
 import {ApartmentBox} from "./ApartmentBox";
+require('jquery');
 
 export class ContentHandler {
     constructor(apartmentData) {
@@ -6,40 +7,76 @@ export class ContentHandler {
         this.defaultLenght = 12;
         this.defaultCardsToLoad = 20;
         this.cardsToLoad = null;
+        this.renderedCardsCount = null;
         this.loadCardsButton = document.querySelector('.apartments__button');
-        this.renderedCards = [];
+        this.priceSortButton = document.querySelector('.apartments__price-sort');
+        this.roomSortButton = document.querySelector('.apartments__rooms-sort');
     }
 
     createContent() {
         for (let i = 0; i < this.defaultLenght; i++) {
             const card = this.aptartmentData[i];
-            this.renderedCards.push(card);
+            this.renderedCardsCount++;
             const apartmentCard = new ApartmentBox(card);
             apartmentCard.render();
         }
         this.loadButtonHandler();
+        this.priceSortButton.addEventListener('click', () =>
+            this.sortHandler(this.priceSortButton, 'price'));
+        this.roomSortButton.addEventListener('click', () =>
+            this.sortHandler(this.roomSortButton, 'roomCount'));
         this.loadCardsButton.addEventListener('click', () => this.loadCards());
     }
 
     loadButtonHandler() {
-        this.cardsToLoad = this.aptartmentData.length - this.renderedCards.length;
+        //this.cardsToLoad = this.aptartmentData.length - this.renderedCards.length;
+        this.cardsToLoad = this.aptartmentData.length - this.renderedCardsCount;
         this.cardsToLoad > this.defaultCardsToLoad ? this.cardsToLoad = this.defaultCardsToLoad : this.cardsToLoad;
 
         if (this.cardsToLoad === 0) {
-            return this.loadCardsButton.style.display = 'none';
+            return this.loadCardsButton.parentNode.removeChild(this.loadCardsButton);
         }
         this.loadCardsButton.innerHTML = `Показать еще ${this.cardsToLoad}`;
     }
 
     loadCards() {
-        const length = this.renderedCards.length + this.cardsToLoad;
+        const length = this.renderedCardsCount + this.cardsToLoad;
 
-        for (let i = this.renderedCards.length; i < length; i++) {
+        for (let i = this.renderedCardsCount; i < length; i++) {
             const card = this.aptartmentData[i];
-            this.renderedCards.push(card);
+            //this.renderedCards.push(card);
+            this.renderedCardsCount++;
             const apartmentCard = new ApartmentBox(card);
             apartmentCard.render();
         }
         this.loadButtonHandler();
+    }
+
+    sortHandler(element, sortParam) {
+        $(element).toggleClass('ascend');
+        $(element).toggleClass('descend');
+
+        $(element).hasClass('descend')
+            ? $(element).find('.apartments__button-circle').addClass('apartments__button-circle_rotate')
+            : $(element).find('.apartments__button-circle').removeClass('apartments__button-circle_rotate');
+
+        $(element).hasClass('ascend')
+            ? this.aptartmentData.sort((a, b) => {return a[sortParam] - b[sortParam]})
+            : this.aptartmentData.sort((a, b) => {return b[sortParam] - a[sortParam]});
+
+        this.renderSortedCards();
+    }
+
+    renderSortedCards() {
+        const cardsContainer = document.querySelector('.apartments__inner-wrapper');
+        while (cardsContainer.firstChild) {
+            cardsContainer.removeChild(cardsContainer.firstChild)
+        }
+
+        for (let i = 0; i < this.renderedCardsCount; i++) {
+            const card = this.aptartmentData[i];
+            const apartmentCard = new ApartmentBox(card);
+            apartmentCard.render();
+        }
     }
 }
--- src/components/content/apartments/request.js
+++ src/components/content/apartments/request.js
@@ -1,15 +1,10 @@
 import {ContentHandler} from './ContentHandler';
-require('jquery');
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 
-$.ajax({
-    type: 'GET',
-    url: 'apt_data/apartments.json',
-    dataType: 'json',
-    success: function (data) {
+fetch('apt_data/apartments.json')
+    .then(response => response.json())
+    .then(data => {
         const handler = new ContentHandler(data);
         handler.createContent();
-    },
-    error: function (errorObj) {
-        console.log(errorObj);
-    }
-});
+    });
